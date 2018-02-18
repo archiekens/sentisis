497073e7a10498e19bca6b3f87fb185eacec8df9
Arnold Christopher Fernando <Fernando_ArnoldC@yahoo.com.ph>
2018-02-18 22:17:54 +0800

    Schema update

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -26,7 +26,7 @@ class AppSchema extends CakeSchema {
 	public $customers = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'email' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 32, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modififed' => array('type' => 'datetime', 'null' => true, 'default' => null),
@@ -55,6 +55,8 @@ class AppSchema extends CakeSchema {
 	public $products = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'type' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false),
+		'brand' => array('type' => 'string', 'null' => false, 'default' => 'Unbranded', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'image' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'rating' => array('type' => 'float', 'null' => false, 'default' => '0', 'unsigned' => false, 'comment' => 'From 0 to 5'),
